e8d2bc8b8d09e5c1f50fef3d0d074412042fe444
jay savani <jaysavani604@gmail.com>
2022-09-06 20:13:41 +0200

    Add: Crypto Layout

---
+++ crypto.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import utilities as ut
+import datetime as dt
+import visualisation as vis
+
+def app():
+    #title of the web app
+    st.title("Crypto Currency Predictions")
+    
+    st.sidebar.subheader("Parameters")
+    start_date = st.sidebar.date_input("Start date", dt.date(2015, 1, 1))
+    end_date = st.sidebar.date_input("End date", dt.date.today())
+    ticker_list = ut.getTickers()
+    tickerSymbol = st.sidebar.selectbox('Coin', ticker_list) # Select ticker symbol
+    
+    
+    # Fetch data
+    df = ut.getTickerData(tickerSymbol,start_date=start_date,end_date=end_date,interval='1d')
+    fig = vis.plotTockenData(df=df, name=tickerSymbol,title="Chart")
+    fig = fig.iplot(asFigure=True)
+    
+    st.plotly_chart(fig)
