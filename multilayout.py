f29459d7d3679545a4682d6ff6a09404b11c3e96
jay savani <jaysavani604@gmail.com>
2022-09-06 20:13:01 +0200

    Fix: SelectionBox

--- multilayout.py
+++ multilayout.py
@@ -20,7 +20,7 @@ class MultiLayout:
         })
     
     def run(self):
-        app = st.sidebar.radio(
+        app = st.selectbox(
             'Categories',
             self.apps,
             format_func = lambda app: app['title']
