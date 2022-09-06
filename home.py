d5cca41cb2eacb915a9078f2a46676aa4bc22d12
jay savani <jaysavani604@gmail.com>
2022-09-06 20:13:24 +0200

    add: Home Layout

---
+++ home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def app():
+    st.title("Crypto Currency Predictions") #title of the web app
+    st.markdown("""
+                This app predicts the price of crypto currency
+                
+                **Credits**
+                - App built by [Jay Savani](https://github.com/jaysavani1)
+                - Built in `Python` using `streamlit`,`yfinance`, `cufflinks`, `pandas` and `datetime`
+                
+                
+                """) #about section
+    st.write("---")
+    st.text_input("", "Search...") #search box
