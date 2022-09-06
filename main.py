969aba8fe32609ff33ad03a0ba7656af121e46d8
jay savani <jaysavani604@gmail.com>
2022-09-06 19:06:11 +0200

    update: Add test funct for demo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from symtable import Symbol
 import pandas as pd
 import numpy as np
 import utilities as ut
@@ -12,6 +13,11 @@ def main():
     
     for lvl in lvls:
         ut.exportAllHistoricalData(level = lvl,export_path=exp_path)
+        
+def test():
+    print("hello")
+    s = ut.getTickerLogo('BTC-USD')
+    print(s)
     
 if __name__ == '__main__':
-    main()
+    test()
