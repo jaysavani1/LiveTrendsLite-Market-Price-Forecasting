894427bd3a1fbb918f813037987a56c6981297ee
jay savani <jaysavani604@gmail.com>
2022-09-28 19:29:20 +0200

    BugFix: Minor Update

--- crypto.py
+++ crypto.py
@@ -6,17 +6,21 @@ import visualisation as vis
 def app():
     #title of the web app
     st.title("Crypto Currency Predictions")
-    
-    st.sidebar.subheader("Parameters")
-    start_date = st.sidebar.date_input("Start date", dt.date(2015, 1, 1))
-    end_date = st.sidebar.date_input("End date", dt.date.today())
+    st.sidebar.caption("Parameters")
+    col1, col2, col3 = st.columns(3)
     ticker_list = ut.getTickers()
-    tickerSymbol = st.sidebar.selectbox('Coin', ticker_list) # Select ticker symbol
+    tickerSymbol = col1.selectbox('Coin', ticker_list) # Select ticker symbol
+    start_date = col2.date_input("Start date", dt.date.today() - dt.timedelta(days=365))
+    end_date = col3.date_input("End date", dt.date.today())
+    st.sidebar.caption("Prediction")
+    future_days = st.sidebar.slider('Days', 1,7,1)
     
-    
-    # Fetch data
+    # Fetch data    
     df = ut.getTickerData(tickerSymbol,start_date=start_date,end_date=end_date,interval='1d')
     fig = vis.plotTockenData(df=df, name=tickerSymbol,title="Chart")
     fig = fig.iplot(asFigure=True)
-    
+
     st.plotly_chart(fig,use_container_width=True)
+    
+    if st.sidebar.button('Predict'):
+        pass
