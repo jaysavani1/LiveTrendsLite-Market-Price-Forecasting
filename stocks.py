2f210b163832495b08e74e83bcc77a7e9774ad29
jay savani <jaysavani604@gmail.com>
2022-10-16 01:21:21 +0200

    Add: Model Prediction added

--- stocks.py
+++ stocks.py
@@ -1,4 +1,5 @@
 from imports import *
+from forecast_model import forecast_bse
 
 def app():
     #title of the web app
@@ -15,24 +16,35 @@ def app():
     st.sidebar.subheader("Parameters") # Sidebar
     stockSymbol = st.sidebar.selectbox('Company', stock_list) # Select company
     timePeriod = st.sidebar.selectbox('Time Duration', period_list) # Select time duration
+    future_days = st.sidebar.slider('Days', 7,90,14)
     
     st.subheader(f"{stockSymbol} - {timePeriod}")
     
     q = b.getPeriodTrend(p[stockSymbol],timePeriod)
     q_df = pd.DataFrame(q)
-    q_df['date'] = pd.to_datetime(q_df['date']).dt.date
-    #st.write(q_df)
+    q_df = q_df.rename({'date':'Date', 'value':'Close','vol':'Volume'},axis=1)
+    q_df['Date'] = pd.to_datetime(q_df['Date']).dt.date
+    print(q_df)
+    st.write(q_df)
     
     col1, col2 = st.columns(2)
 
     col1.markdown("<h3 style='text-align: center;'>Price</h3>", unsafe_allow_html=True)
     col1.line_chart(data = q_df,
-                  x = 'date',
-                  y = 'value')
+                  x = 'Date',
+                  y = 'Close')
     col2.markdown("<h3 style='text-align: center;'>Volume</h3>", unsafe_allow_html=True)
     col2.bar_chart(data = q_df,
-                    x = 'date',
-                    y = 'vol')
+                    x = 'Date',
+                    y = 'Volume')
+    
+    if st.sidebar.button('Predict'):
+        df_forecast = forecast_bse(q_df, days = future_days)
+        st.area_chart(df_forecast,  
+                      x = 'Date',
+                      y = 'Close',
+                      #width = 5.5,
+                      use_container_width=True)
     
     
     #start_date = st.sidebar.date_input("Start date", dt.date(2015, 1, 1))
